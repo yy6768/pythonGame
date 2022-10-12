8f6745f70028d2c7481a09690173982a4779e27b
yyorange <1258720926@qq.com>
2022-10-12 23:11:18 +0800

    day4开发 玩家绘制完成

--- VallyGame/code/player.py
+++ VallyGame/code/player.py
@@ -15,7 +15,7 @@ class Player(pygame.sprite.Sprite):
         # 加载所有动画
         self.import_assets()
         # 格式：方向+状态（状态可以为空） exp：down（没状态的） down_axe(方向向下，状态为axe锄头
-        self.status = 'down'
+        self.status = 'down_idle'
         self.frame_index = 0
         # 一般的设置
         # 玩家的图片，需要一个可绘制的surface
@@ -40,25 +40,44 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
         print(self.animations)
 
+    # 当前动画
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
+    # 玩家的输入
     def input(self):
         keys = pygame.key.get_pressed()  # 键盘按下的键
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         else:
             self.direction.x = 0
 
+    def get_status(self):
+        # 如果玩家没有移动
+        # add idle 到状态结尾
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split('_')[0] + '_idle'
+
     def move(self, dt):
         # 将方向向量normalization
-        if self.direction.magnitude():
+        if self.direction.magnitude() > 0:
             self.direction = self.direction.normalize()
         print(self.direction)
 
@@ -72,4 +91,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
